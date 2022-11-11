7b25ae76071876fcc0e89535bf7963596cc52669
efrgdc <coinus@naver.com>
2022-11-11 11:43:47 +0900

    블랙홀 추가
    하지만 미완성

---
+++ Assets/Scripts/BlackHole/BlackHoleMove.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class BlackHoleMove : MonoBehaviourPun
+{
+    [SerializeField] private List<GameObject> blackHoleList = null;
+    private float blackHoleTime = 10f;
+    private float time = 0;
+    private int MasterRan;
+    private int ran = 0;
+
+    private void Start()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            photonView.RPC("BlackHolePos", RpcTarget.MasterClient, MasterRan);
+        }
+    }
+
+    [PunRPC]
+    public void BlackHolePos(int MasterRan)
+    {
+        photonView.RPC("RandomBlackHole", RpcTarget.All, MasterRan);
+    }
+
+    [PunRPC]
+    public void RandomBlackHole()
+    {
+        if (blackHoleList.Count > 0)
+        {
+            MasterRan = Random.Range(0, blackHoleList.Count);
+            ran = MasterRan;
+        }
+    }
+
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time >= blackHoleTime)
+        {
+            RandomBlackHole();
+            blackHoleList[ran].SetActive(true);
+            blackHoleList.Remove(blackHoleList[ran]);
+            time = 0;
+        }
+    }
+}
