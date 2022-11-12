76c9398cd537576b8ae2cdc4d4a945b531fd4070
KingOneChance <111823096+KingOneChance@users.noreply.github.com>
2022-11-12 14:40:15 +0900

    BID_1chance
    update

--- Assets/Scripts/Player/PlayerInfo.cs
+++ Assets/Scripts/Player/PlayerInfo.cs
@@ -107,6 +107,7 @@ public struct DamageInfo
         playerAlive = state.Die;
         GameMgr.Instance.gameSceneLogic.AliveNumCheck();
     }
+    //테스트 죽음용   
     [PunRPC]
     private void Test_Die()
     {
