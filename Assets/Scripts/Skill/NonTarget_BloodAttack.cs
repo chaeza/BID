4779ab818c47ec2b0cf46d18e8758ac82e951816
Lucid-Kim <kykab1991@gmail.com>
2022-11-15 11:38:10 +0900

    Yeongkab
    skill add two

---
+++ Assets/Scripts/Skill/NonTarget_BloodAttack.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NonTarget_BloodAttack : Skill
+{
+    public void SetSkillNum(int Num)
+    {
+        skillInfo.skillNum = Num;
+    }
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        skillInfo.type = SkillType.Skill;
+        skillInfo.radius = 3;//use Immediate,NonTarget
+        skillInfo.range = 15;//use Projectile,NonTarget,Cone
+        skillInfo.cooltime = 15;
+        skillInfo.skillType = SkillType.NonTarget;
+
+        skillInfo.hitBoxInfo.attackType = AttackType.Shot;
+        skillInfo.hitBoxInfo.interval = 0.5f;
+
+        skillInfo.hitBoxInfo.damageInfo.attackState = state.None;
+        skillInfo.hitBoxInfo.damageInfo.attackDamage = 10;
+        skillInfo.hitBoxInfo.damageInfo.attackerViewID = gameObject.GetPhotonView().ViewID;
+        skillInfo.hitBoxInfo.damageInfo.slowDownRate = 0;
+        skillInfo.hitBoxInfo.damageInfo.timer = 1f;
+    }
+
+    private void Update()
+    {
+        if (GameMgr.Instance.playerInput.inputKey == KeyCode.D) SkillUse();
+        else if (GameMgr.Instance.playerInput.inputKey == KeyCode.Mouse0) SkillClick(Input.mousePosition);
+    }
+    protected override void SkillFire()
+    {
+        playerInfo.StayPlayer(0.5f);
+        animator.SetTrigger("isSkill2");
+        transform.LookAt(desiredDir);
+        StartCoroutine(SkillFire_Delay(0.5f));
+        if (skillInfo.cooltime != 0) GameMgr.Instance.uIMgr.SkillCooltime(skillInfo.cooltime, skillInfo.skillNum, 0);
+    }
+    IEnumerator SkillFire_Delay(float time)
+    {
+
+        yield return new WaitForSeconds(time);
+        GameObject eff = PhotonNetwork.Instantiate("BloodAttack", desiredDir, Quaternion.identity);
+        eff.transform.Rotate(-90, 0f, 0);
+        eff.AddComponent<HitBox>().skillInfo = skillInfo;
+        GameMgr.Instance.DestroyTarget(eff, 5f);
+
+    }
+}
--- Assets/Scripts/Skill/RandomSkill.cs
+++ Assets/Scripts/Skill/RandomSkill.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class RandomSkill : MonoBehaviour
 {
     //total number of skills
-    private int skillType = 5;
+    private int skillType = 7;
     //skills random
     public int skillRan { get; private set; } = 0;
     //Distributing random skills
@@ -13,12 +13,15 @@ public class RandomSkill : MonoBehaviour
     {
         //Random skill number
         skillRan = Random.Range(1, skillType+1);
+        skillRan = 7;
         GameMgr.Instance.uIMgr.SetSkillIcon(skillRan,0);
         if (skillRan == 1) player.AddComponent<Immediate_BloodField>().SetSkillNum(skillRan);
         else if (skillRan == 2) player.AddComponent<Projectile_Bash>().SetSkillNum(skillRan);
         else if (skillRan == 3) player.AddComponent<NonTarget_FallTheRain>().SetSkillNum(skillRan);
         else if (skillRan == 4) player.AddComponent<Projectile_MysticArrow>().SetSkillNum(skillRan);
         else if (skillRan == 5) player.AddComponent<Projectile_SwordStab>().SetSkillNum(skillRan);
+        else if (skillRan == 6) player.AddComponent<Immediate_DevilSword>().SetSkillNum(skillRan);
+        else if (skillRan == 7) player.AddComponent<NonTarget_BloodAttack>().SetSkillNum(skillRan);
         else
             Debug.Log("Player didn't get any skill");
     }
