bb1dcb6de5390501502074903fb4eb5565b4b879
efrgdc <coinus@naver.com>
2022-11-16 20:16:30 +0900

    ㄷ
    ㄷ

--- Assets/Scripts/LodingSync.cs
+++ Assets/Scripts/LodingSync.cs
@@ -21,10 +21,10 @@ public class LodingSync : MonoBehaviourPunCallbacks
 
     void Start()
     {
-        //if (PhotonNetwork.IsMasterClient)
-        //    photonView.StartCoroutine(LoadAsynSceneCoroutine());
+        if (PhotonNetwork.IsMasterClient)
+            photonView.StartCoroutine(LoadAsynSceneCoroutine());
 
-        //else
+        else
             photonView.StartCoroutine(PlayerSceneCoroutine());
     }
 
